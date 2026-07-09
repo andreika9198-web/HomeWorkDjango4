9ab482c60badbf4bad9e7c814b49ec2975d8c4de
Sapranov Andrey <andreika9198@gmail.com>
2026-07-09 09:08:48 +0300

    - Приложение gemes добавлено в настройки проекта - Добавлена модель Genre; - Созданы и проведены миграции;

--- games/models.py
+++ games/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from users.models import  NULLABLE
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100, verbose_name='genre')
+    description = models.CharField(max_length=1000, verbose_name='description', **NULLABLE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'genre'
+        verbose_name_plural = 'genres'
+
