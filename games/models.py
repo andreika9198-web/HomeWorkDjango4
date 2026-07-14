279cd5ab2ad486efa253ce1e16b187821f05bec6
Sapranov Andrey <andreika9198@gmail.com>
2026-07-14 15:17:53 +0300

    -Добавлена часть шаблонов для уже написанных view -Переписаны название полей отображаемых на фронт по созданию игр

--- games/models.py
+++ games/models.py
@@ -3,8 +3,8 @@ from django.db import models
 from users.models import  NULLABLE
 
 class Genre(models.Model):
-    name = models.CharField(max_length=100, verbose_name='genre')
-    description = models.CharField(max_length=1000, verbose_name='description', **NULLABLE)
+    name = models.CharField(max_length=100, verbose_name='Жанр')
+    description = models.CharField(max_length=1000, verbose_name='Описание', **NULLABLE)
 
     def __str__(self):
         return self.name
@@ -14,10 +14,10 @@ class Genre(models.Model):
         verbose_name_plural = 'genres'
 
 class Game(models.Model):
-    name = models.CharField(max_length=250, verbose_name='game_name')
-    genre = models.ForeignKey(Genre, on_delete=models.CASCADE, verbose_name='genre')
-    photo = models.ImageField(upload_to='games/', **NULLABLE, verbose_name='photo')
-    date_of_publication = models.DateField(**NULLABLE, verbose_name='date_of_publication')
+    name = models.CharField(max_length=250, verbose_name='Название игры')
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE, verbose_name='Жанр')
+    photo = models.ImageField(upload_to='games/', **NULLABLE, verbose_name='Фото')
+    date_of_publication = models.DateField(**NULLABLE, verbose_name='Дата выхода')
 
     def __str__(self):
         return f'{self.name} ({self.genre})'
