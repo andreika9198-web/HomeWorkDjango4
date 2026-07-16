eeb040c477acb9016a8009e12e017cba011fa0da
Sapranov Andrey <andreika9198@gmail.com>
2026-07-16 09:27:12 +0300

    - Написан функционал регистрации пользователя

--- users/admin.py
+++ users/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from users.models import User
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('pk','last_name', 'first_name',)
+    list_display = ('pk', 'email','last_name', 'first_name',)
     search_fields = ('last_name',)
     # list_filter = ('is_staff', 'is_active')
     # ordering = ('id',)
--- users/views.py
+++ users/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
+from django.http import  HttpResponseRedirect
 
-# Create your views here.
+from  users.forms import  UserRegisterForm
+
+def user_register_view(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            new_user.set_passsword(form.cleaned_data['password'])
+            new_user.save()
+            return HttpResponseRedirect(reverse('games:index'))
+    context = {
+        'title': 'Регистрация',
+        'form': UserRegisterForm
+    }
+    return render(request, 'users/register.html', context=context)
+
